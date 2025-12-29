e72370644b1043452e9d186ec50e958c0e840915
GallYana <rowdy_69@bk.ru>
2025-12-29 23:13:27 +0500

    +

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist'
+  },
   assetsInclude: ['**/*.hbs'],
   server: {
     port: 3000,
