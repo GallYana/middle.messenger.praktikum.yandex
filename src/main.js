1f8fc2631097556671e01b57d840696c593154ce
GallYana <rowdy_69@bk.ru>
2025-12-29 22:49:34 +0500

    Почти готово

--- src/main.js
+++ src/main.js
@@ -1,19 +1,127 @@
-import './style.css'
+import './fonts.pcss';
+import './style.pcss';
+import Handlebars from 'handlebars';
+import linkTemplateRaw from './partial/links.hbs?raw';
+import errorTemplateRaw from './partial/error.hbs?raw';
+import loginTemplateRaw from './partial/form.hbs?raw';
+import registerTemplateRaw from './partial/form__register.hbs?raw';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+data: {
+  footer: `<footer class="footer">
+    <a class="logo__container" href="/">
+    <img alt="Логотип" class="logo-icon" src="/static/img/paw-icon.svg" />
+    <span class="display gray__color">paw_messenger</span>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+</footer>`
+}
+
+const PAGES = {
+  '/': {
+    template: Handlebars.compile(linkTemplateRaw),
+    data: { 
+      wrapperClassName: 'center__container', links: ["error__404", "error__500", "login", "register"] 
+    }
+  },
+  '/register': {
+    template: Handlebars.compile(registerTemplateRaw),
+    data: { 
+      wrapperClassName: 'login__container center__container', title: 'Регистрация',
+      formItems: [
+        {
+          type: "text",
+          label: "Логин",
+          inputValue: "login",
+          placeholder: "Введите логин"
+        },
+
+        {
+          type: "text",
+          label: "Имя",
+          inputValue: "first_name",
+          placeholder: "Введите имя"
+        },
+
+        {
+          type: "text",
+          label: "Фамилия",
+          inputValue: "second_name",
+          placeholder: "Введите фамилию"
+        },
+
+        {
+          type: "email",
+          label: "Почта",
+          inputValue: "email",
+          placeholder: "Введите электронную почту"
+        },
+
+        {
+          type: "phone",
+          label: "Телефон",
+          inputValue: "phone",
+          placeholder: "Введите номер телефона"
+        },
+
+        {
+          type: "password",
+          label: "Пароль",
+          inputValue: "password",
+          placeholder: "Введите пароль"
+        },
+
+        {
+          type: "password",
+          label: "Повторите пароль",
+          inputValue: "repeat_password",
+          placeholder: "Введите пароль"
+        }
+      ] 
+    }
+  },
+  '/login': {
+    template: Handlebars.compile(loginTemplateRaw),
+    data: { 
+      wrapperClassName: 'login__container center__container', title: 'Вход',
+      formItems: [
+        {
+          type: "text",
+          label: "Логин",
+          inputValue: "login",
+          placeholder: "Введите логин"
+        },
+
+        {
+          type: "password",
+          label: "Пароль",
+          inputValue: "password",
+          placeholder: "Введите пароль"
+        },
+      ] 
+    }
+  },
+  '/error__404': {
+    template: Handlebars.compile(errorTemplateRaw),
+    data: { 
+      wrapperClassName: 'error__container center__container', error: '404', errorText: 'Не удалось найти страницу по указанному адресу' 
+    }
+  },
+  '/error__500': {
+    template: Handlebars.compile(errorTemplateRaw),
+    data: { 
+      wrapperClassName: 'error__container center__container', error: '500', errorText: 'Произошла непредвиденная ошибка, попробуйте перезагрузить страницу' 
+    }
+  }
+};
+
+function getCurrentPage() {
+  const path = window.location.pathname;
+  return PAGES[path] ? path : '/';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const currentPath = getCurrentPage();
+  const pageConfig = PAGES[currentPath];
+  document.querySelector('#app').innerHTML = pageConfig.template(pageConfig.data);
+});
+
+console.log(currentUrl);
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
+export default defineConfig({
+  assetsInclude: ['**/*.hbs'],
+  server: {
+    port: 3000,
+  },
+  css: {
+    postcss: {
+      config: './postcss.config.json',
+    },
+  },
+  plugins: [
+    handlebars({
+      // partials — если будут {{> partialName }}
+      partialDirectory: resolve(__dirname, 'src/partial'),
+      // runtime можно не указывать — плагин подключит handlebars/runtime сам
+    }),
+  ],
+});
