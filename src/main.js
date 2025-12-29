be5cf00494599d910b959d0c3e9b40541fee21ee
GallYana <rowdy_69@bk.ru>
2025-12-29 23:23:27 +0500

    +

--- src/main.js
+++ src/main.js
@@ -9,5 +9,10 @@ import registerTemplateRaw from './partial/form__register.hbs?raw';
 // template: Handlebars.compile(linkTemplateRaw)
 
 document.querySelector('#app').innerHTML = `
-
+<footer class="footer">
+      <a class="logo__container" href="/">
+        <img alt="Логотип" class="logo-icon" src="/static/img/paw-icon.svg" />
+        <span class="display gray__color">paw_messenger</span>
+      </div>
+    </footer>
 `
